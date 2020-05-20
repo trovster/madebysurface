f7b17835e9f041a2dc113af63418e85f20e526b0
Trevor Morris <code@trovster.com>
2020-05-20 22:23:48 +0100

    ci: base URLs for netlify

--- src/_data/site.js
+++ src/_data/site.js
@@ -19,8 +19,19 @@ module.exports = () => {
     site.baseUrl = process.env.ELEVENTY_BASE_URL
   }
 
+  // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+  if (process.env.URL) {
+    site.baseUrl = process.env.URL
+  }
+
+  // https://vercel.com/docs/v2/build-step#system-environment-variables
   if (process.env.VERCEL_URL) {
-    site.baseUrl = `https://${process.env.VERCEL_URL}`
+    site.baseUrl = process.env.VERCEL_URL
+  }
+
+  // https://help.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables
+  if (process.env.GITHUB_URL) {
+    site.baseUrl = process.env.GITHUB_URL
   }
 
   return site
