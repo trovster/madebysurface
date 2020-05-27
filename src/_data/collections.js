79eb16b781311963783eeb542dc8e8dff6390d8b
Trevor Morris <code@trovster.com>
2020-05-27 17:11:03 +0100

    fix: order of testimonials

--- src/_data/collections.js
+++ src/_data/collections.js
@@ -3,6 +3,19 @@ module.exports = {
     return collection.getFilteredByGlob(['./src/expertise/*.md'])
   },
   testimonials: (collection) => {
-    return collection.getFilteredByGlob(['./src/testimonials/*.md'])
+    return collection.getFilteredByGlob(['./src/testimonials/*.md']).sort((a, b) => {
+      const aNumber = parseInt(a.data.page.fileSlug, 10)
+      const bNumber = parseInt(b.data.page.fileSlug, 10)
+
+      if (aNumber < bNumber) {
+        return -1
+      }
+
+      if (aNumber > bNumber) {
+        return 1
+      }
+
+      return 0
+    })
   }
 }
