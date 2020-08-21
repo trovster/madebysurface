801b833e42be66ad5f31f40c5194a95fce293954
Trevor Morris <code@trovster.com>
2020-08-21 16:05:23 +0100

    fix: use order for sorting

--- src/_data/collections.js
+++ src/_data/collections.js
@@ -4,8 +4,8 @@ module.exports = {
   },
   testimonials: (collection) => {
     return collection.getFilteredByGlob(['./src/testimonials/*.md']).sort((a, b) => {
-      const aNumber = parseInt(a.data.page.fileSlug, 10)
-      const bNumber = parseInt(b.data.page.fileSlug, 10)
+      const aNumber = a.data.order || parseInt(a.data.page.fileSlug, 10)
+      const bNumber = b.data.order || parseInt(b.data.page.fileSlug, 10)
 
       if (aNumber < bNumber) {
         return -1
