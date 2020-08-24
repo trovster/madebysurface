ea33e0631f34e555f37a25590dc2c3aff48cf0ad
trovster <code@trovster.com>
2020-08-24 09:16:11 +0000

    Deploying to gh-pages from  @ 15648c036172119d10b7bdcf901b43183a6c69fb 🚀

---
+++ admin/previews.js
@@ -0,0 +1,46 @@
+/* global CMS, h, createClass */
+const TestimonialPreview = createClass({
+  render: function () {
+    const entry = this.props.entry
+    const author = entry.getIn(['data', 'author'])
+    const company = entry.getIn(['data', 'org'])
+
+    return h('div', { className: 'testimonial' },
+      h('blockquote', {}, this.props.widgetFor('body')),
+      h('p', {},
+        h('cite', { className: 'vcard' },
+          h('span', { className: 'fn author' }, company ? `${author}, ` : author),
+          h('span', { className: 'org' }, company || '')
+        )
+      )
+    )
+  }
+})
+
+const ExpertisePreview = createClass({
+  render: function () {
+    const entry = this.props.entry
+    const title = entry.getIn(['data', 'title'])
+
+    return h('div', { className: 'expertise' },
+      h('h2', {}, title),
+      h('div', {}, this.props.widgetFor('body'))
+    )
+  }
+})
+
+const PlaygroundPreview = createClass({
+  render: function () {
+    const entry = this.props.entry
+    const title = entry.getIn(['data', 'title'])
+
+    return h('div', { className: 'playground' },
+      h('h2', {}, title)
+      // h('div', {}, this.props.widgetFor('body'))
+    )
+  }
+})
+
+CMS.registerPreviewTemplate('testimonials', TestimonialPreview)
+CMS.registerPreviewTemplate('expertise', ExpertisePreview)
+CMS.registerPreviewTemplate('playground', PlaygroundPreview)
